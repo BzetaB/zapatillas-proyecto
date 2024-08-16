e5a1a64fc9e11b1cca61386bb8260012ca0ae993
bzetab <zevallosbenjamin100@gmail.com>
2024-08-16 00:14:55 -0500

    creacion de proyecto

---
+++ src/main/java/com/zapatillas/proyecto/ProyectoZapatillasApplication.java
@@ -0,0 +1,13 @@
+package com.zapatillas.proyecto;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ProyectoZapatillasApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ProyectoZapatillasApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/zapatillas/proyecto/ProyectoZapatillasApplicationTests.java
@@ -0,0 +1,13 @@
+package com.zapatillas.proyecto;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ProyectoZapatillasApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
