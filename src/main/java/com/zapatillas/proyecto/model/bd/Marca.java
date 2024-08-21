de62a3786f1019e46add2fd0ce5cb9568037420b
unknown <tumasterdiego1@gmail.com>
2024-08-21 18:41:28 -0500

    Marca model

--- src/main/java/com/zapatillas/proyecto/model/bd/Marca.java
+++ src/main/java/com/zapatillas/proyecto/model/bd/Marca.java
@@ -1,4 +1,18 @@
 package com.zapatillas.proyecto.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "marca")
 public class Marca {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idmarca;
+
+    private String nombre;
+    private String descripcion;
 }
