b95811a6bae2bc7df3fe60be15b1e601b9d87d1b
unknown <tumasterdiego1@gmail.com>
2024-08-21 14:26:06 -0500

    Apartado Service de Provedor

---
+++ src/main/java/com/zapatillas/proyecto/service/IProvedorService.java
@@ -0,0 +1,12 @@
+package com.zapatillas.proyecto.service;
+
+import com.zapatillas.proyecto.model.bd.Provedor;
+
+import java.util.List;
+
+public interface IProvedorService {
+    List<Provedor> listarProvedores();
+    Provedor guardarProvedor(Provedor provedor);
+    Provedor buscarProvedorPorId(Integer id);
+    void eliminarProvedor(Integer id);
+}
---
+++ src/main/java/com/zapatillas/proyecto/service/ProvedorService.java
@@ -0,0 +1,38 @@
+package com.zapatillas.proyecto.service;
+
+import com.zapatillas.proyecto.model.bd.Provedor;
+import com.zapatillas.proyecto.repository.ProvedorRepository;
+import lombok.AllArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.List;
+
+@AllArgsConstructor
+@Service
+public class ProvedorService implements  IProvedorService{
+    private final ProvedorRepository provedorRepository;
+
+    @Override
+    public List<Provedor> listarProvedores() {
+        return provedorRepository.findAll();
+    }
+
+    @Override
+    public Provedor guardarProvedor(Provedor provedor) {
+        return provedorRepository.save(provedor);
+    }
+
+    @Override
+    public Provedor buscarProvedorPorId(Integer id) {
+        return provedorRepository.findById(id).orElse(null);
+    }
+
+    @Override
+    public void eliminarProvedor(Integer id) {
+        if (provedorRepository.existsById(id)) {
+            provedorRepository.deleteById(id);
+        } else {
+            throw new RuntimeException("Provedor con ID " + id + " no encontrado");
+        }
+    }
+}
