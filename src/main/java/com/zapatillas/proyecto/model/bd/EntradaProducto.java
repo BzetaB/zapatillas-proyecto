51b52deb425fa6143cbab83acbff200a2ff64eca
unknown <tumasterdiego1@gmail.com>
2024-08-21 14:45:36 -0500

    Model EntradaProducto

--- src/main/java/com/zapatillas/proyecto/model/bd/EntradaProducto.java
+++ src/main/java/com/zapatillas/proyecto/model/bd/EntradaProducto.java
@@ -1,4 +1,24 @@
 package com.zapatillas.proyecto.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "entrada_producto")
 public class EntradaProducto {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idetdpro;
+
+    private String etdpronombre;
+    private String etdprodescripcion;
+    private Double etdproprecio;
+    private Integer etdprostock;
+
+    @ManyToOne
+    @JoinColumn(name = "idprovedor")
+    private Provedor proveedor;
 }
