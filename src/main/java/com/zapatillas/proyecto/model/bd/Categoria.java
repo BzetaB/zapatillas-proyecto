84176ddab11d0eb1570d1fa5d223cedafd17104d
unknown <tumasterdiego1@gmail.com>
2024-08-21 19:27:01 -0500

    Categoria Model

--- src/main/java/com/zapatillas/proyecto/model/bd/Categoria.java
+++ src/main/java/com/zapatillas/proyecto/model/bd/Categoria.java
@@ -1,4 +1,18 @@
 package com.zapatillas.proyecto.model.bd;
 
+import jakarta.persistence.*;
+import lombok.Getter;
+import lombok.Setter;
+
+@Getter
+@Setter
+@Entity
+@Table(name = "categoria")
 public class Categoria {
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Integer idcategoria;
+
+    private String nombre;
 }
+
